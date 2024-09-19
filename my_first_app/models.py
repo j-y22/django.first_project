1738c738f973a2f54d649309969ce6a98de255d1
jiali.yang <jiali.yang@upc.edu>
2024-09-19 10:20:10 +0200

    esto es una prueba

--- my_first_app/models.py
+++ my_first_app/models.py
@@ -4,3 +4,7 @@ from django.db import models
 class Car(models.Model):
     title=  models.TextField(max_length= 250)
     year = models.TextField(max_length=4, null=True)
+    color = models.TextField(max_length=10, null=True)
+    
+    def __str__(self):
+        return f"{self.title} - {self.year} - {self.color}"
